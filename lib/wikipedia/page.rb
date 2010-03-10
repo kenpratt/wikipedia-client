f375d2ec1bef64fd69adc7e216ab0472a924db5f
Ken Pratt <ken@kenpratt.net>
2010-03-10 14:21:32 -0800

    Added retrieval of categories and links.

--- lib/wikipedia/page.rb
+++ lib/wikipedia/page.rb
@@ -4,27 +4,35 @@ module Wikipedia
       require 'json'
       @data = JSON::load(json)
     end
-    
+
     def content
       @data['query']['pages'].values.first['revisions'].first.values.first
     end
-    
+
     def redirect?
       content.match(/\#REDIRECT\s+\[\[(.*?)\]\]/)
     end
-    
+
     def redirect_title
       if matches = redirect?
         matches[1]
       end
     end
-    
+
     def title
       @data['query']['pages'].values.first['title']
     end
 
+    def categories
+      @data['query']['pages'].values.first['categories'].map {|c| c['title'] }
+    end
+
+    def links
+      @data['query']['pages'].values.first['links'].map {|c| c['title'] }
+    end
+
     def images
-      @data['query']['pages'].values.first['images']
+      @data['query']['pages'].values.first['images'].map {|c| c['title'] }
     end
 
     def image_url
@@ -32,8 +40,8 @@ module Wikipedia
     end
 
     def image_urls
-      images.map do |i|
-        Wikipedia.find_image( i['title'] ).image_url
+      images.map do |title|
+        Wikipedia.find_image( title ).image_url
       end
     end
 
