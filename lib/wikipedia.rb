982b7554f587b65b218aa49fe8ec0780fc95b7d2
indeep-xyz <indeep.xyz@gmail.com>
2014-12-23 22:48:37 +0900

    add feature for find a random page.
    lib/Wikipedia.find_random
      alias for Wikipedia::Client.find_random.
    
    lib/Wikipedia::Client.find_random
      get Wikipedia::Page object that take at random from the Wikipedia.
      use Wikipedia::Client.request_random method.
    
    lib/Wikipedia::Client.request_random
      get JSON string for random page's data by Wikipedia API.
    
    spec/lib/client_spec.rb
      test for Wikipedia::Client.find_random.

--- lib/wikipedia.rb
+++ lib/wikipedia.rb
@@ -3,13 +3,13 @@ Dir[File.dirname(__FILE__) + '/wikipedia/**/*.rb'].each { |f| require f }
 require 'uri'
 
 module Wikipedia
-  # Examples : 
-  # page = Wikipedia.find('Rails') 
+  # Examples :
+  # page = Wikipedia.find('Rails')
   # => #<Wikipedia:0x123102>
   # page.content
   # => wiki content appears here
-  
-  # basically just a wrapper for doing 
+
+  # basically just a wrapper for doing
   # client = Wikipedia::Client.new
   # client.find('Rails')
   #
@@ -19,11 +19,14 @@ module Wikipedia
   def self.find_image( title, options = {} )
     client.find_image( title, options )
   end
-  
+  def self.find_random( options = {} )
+    client.find_random( options )
+  end
+
   def self.Configure(&block)
     Configuration.instance.instance_eval(&block)
   end
-  
+
   Configure {
     protocol  'http'
     domain    'en.wikipedia.org'
