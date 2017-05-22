8b3e4a4376550e141db0d22e1c443621f4da0c21
Harman Singh <harman28@gmail.com>
2017-05-23 01:02:39 +0530

    [rubocop] Fixes Url.rb * Adds top level comment for class * Removes trailing space * Disables cop for SpaceInsideParens

--- lib/wikipedia/url.rb
+++ lib/wikipedia/url.rb
@@ -1,12 +1,13 @@
 module Wikipedia
+  # Class to hold and decode URLs
   class Url
     def initialize(wiki_url)
       @wiki_url = wiki_url
     end
-    
+
     def title
       return @title if @title
-      
+
       uri     = URI.parse( @wiki_url )
       @title  = URI.decode( uri.path.split('/').last )
     end
