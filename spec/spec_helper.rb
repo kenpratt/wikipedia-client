572421670fb7b0d06c4b6e2ae86f19a1dc39dd5e
Harman Singh <harman28@gmail.com>
2017-05-24 03:19:13 +0530

    [rake] Improves the Rakefile * Replaces rcov with simplecov * Uses default rake spec definition, so avoid exec calls * Removes unnecessary rubygems requirement * Removes unnecessary tasks

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,12 @@
-require 'rubygems'
 require 'rspec'
+
+if ENV['COVERAGE'] == 'true'
+  begin
+    require 'simplecov'
+    SimpleCov.start
+  rescue LoadError
+    abort 'Coverage driver not available. In order to run coverage, you must run: gem install simplecov'
+  end
+end
 
 require File.dirname(__FILE__) + '/../lib/wikipedia'
